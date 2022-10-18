9f4175a5da67c70a7758e0c7900ed3362f32071b
Kacper Serewiś <kacper@kacperserewis.net>
2022-10-19 01:08:37 +0200

    remove status from eventmodel

--- src/models/event.model.ts
+++ src/models/event.model.ts
@@ -4,14 +4,14 @@ export class EventModel implements IEvent {
 
     constructor(public data: {
         id: string,
-        status: string,
         created: Date,
         updated: Date,
         summary: string,
         creator: string,
         startDateTime: Date,
         endDateTime: Date,
-        timeZone: string
+        timeZone: string,
+        addedFrom?: string
     }
     ) {
 
@@ -20,7 +20,6 @@ export class EventModel implements IEvent {
     static fromNotionDatabaseObject(notionDatabaseObject: any): EventModel {
         return new EventModel({
             id: notionDatabaseObject.id,
-            status: notionDatabaseObject.properties.Status.status.name,
             created: new Date(notionDatabaseObject.created_time),
             updated: new Date(notionDatabaseObject.last_edited_time),
             summary: notionDatabaseObject.properties.Name.title[0].plain_text,
@@ -34,22 +33,21 @@ export class EventModel implements IEvent {
 
 
     static isValidNotionDatabaseObject(notionDatabaseObject: any): boolean {
-        const basicChecks = notionDatabaseObject.id && notionDatabaseObject.created_time && notionDatabaseObject.last_edited_time;
 
+        console.log(JSON.stringify(notionDatabaseObject, null, 2))
+        const basicChecks = notionDatabaseObject.id && notionDatabaseObject.created_time && notionDatabaseObject.last_edited_time;
         const propertyKeys = Object.keys(notionDatabaseObject.properties);
         const nameChecks = propertyKeys.includes("Name") && notionDatabaseObject.properties.Name.title.length > 0 && notionDatabaseObject.properties.Name.title[0].plain_text;
         const dateChecks = propertyKeys.includes("Date") && notionDatabaseObject.properties.Date.date;
-        const statusChecks = propertyKeys.includes("Status") && notionDatabaseObject.properties.Status.status.name;
         // const descriptionChecks = propertyKeys.includes("Description") && notionDatabaseObject.properties.Description.rich_text.length > 0 && notionDatabaseObject.properties.Description.rich_text[0].plain_text;
 
 
-        return basicChecks && nameChecks && dateChecks && statusChecks;
+        return basicChecks && nameChecks && dateChecks;
     }
 
     static fromGoogleEvent(googleEvent: any): EventModel {
         return new EventModel({
             id: googleEvent.id,
-            status: googleEvent.status,
             created: new Date(googleEvent.created),
             updated: new Date(googleEvent.updated),
             summary: googleEvent.summary,
