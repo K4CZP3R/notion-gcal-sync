98ac2e9de176688068b00d1904155b7818fafa2e
Kacper Serewiś <kacper@kacperserewis.net>
2022-10-19 01:47:30 +0200

    add add and remove endpoints

--- src/controllers/base-cal.controller.ts
+++ src/controllers/base-cal.controller.ts
@@ -16,14 +16,36 @@ export class BaseCalController extends BaseController {
             func: this.getEventsOfCalendar.bind(this),
         },
         {
-            path: "/calendar/:calendarId/:eventId",
+            path: "/calendar/:calendarId/event/:eventId",
             method: "GET",
             func: this.getEventOfCalendar.bind(this),
+        },
+        {
+            path: "/calendar/:calendarId/event",
+            method: "POST",
+            func: this.createEventInCalendar.bind(this),
+        },
+        {
+            path: "/calendar/:calendarId/event/:eventId",
+            method: "DELETE",
+            func: this.deleteEventOfCalendar.bind(this),
         }
     ];
     constructor(path: string, private calService: ICalService) {
         super({ path });
         this.loadRoutes();
+    }
+
+    async deleteEventOfCalendar(req: Request, res: Response, next: NextFunction) {
+        const { calendarId, eventId } = req.params;
+        await this.calService.deleteEvent(calendarId, eventId);
+        res.json({ success: true });
+    }
+
+    async createEventInCalendar(req: Request, res: Response, next: NextFunction) {
+        const { calendarId } = req.params;
+        const event = await this.calService.createEvent(calendarId, req.body);
+        res.json(event.data);
     }
 
     async getEventOfCalendar(req: Request, res: Response, next: NextFunction) {
