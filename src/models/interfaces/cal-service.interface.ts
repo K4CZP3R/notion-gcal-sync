131d7ab3df8430e9e9d94465f5d2299342804e4e
Kacper Serewiś <kacper@kacperserewis.net>
2022-10-19 01:48:02 +0200

    add create/delete in cal service

--- src/models/interfaces/cal-service.interface.ts
+++ src/models/interfaces/cal-service.interface.ts
@@ -5,4 +5,6 @@ export interface ICalService {
     getEvents(calendarId: string): Promise<IEvent[]>;
     getCalendars(): Promise<ICalendar[]>;
     getEvent(calendarId: string, eventId: string): Promise<IEvent>;
+    createEvent(calendarId: string, event: { summary: string, startDateTime: string, endDateTime: string, description: string }): Promise<IEvent>;
+    deleteEvent(calendarId: string, eventId: string): Promise<void>
 }
